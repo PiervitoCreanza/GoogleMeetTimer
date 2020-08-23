2e8c33f280e23bd0335dd731521876bdf6d02e07
PiervitoCreanza <p.creanza@protonmail.com>
2020-08-23 12:20:37 +0200

    Display manual on installation

--- extension/src/background.js
+++ extension/src/background.js
@@ -1,1 +1,8 @@
 chrome.runtime.setUninstallURL('https://timer.digitilab.it/uninstall')
+
+// Check whether new version is installed
+chrome.runtime.onInstalled.addListener(function(details){
+    if(details.reason == "install"){
+        chrome.tabs.create({ url: "https://timer.digitilab.it/download/GoogleMeetTimer_Manual_en.pdf" })
+    }
+});
